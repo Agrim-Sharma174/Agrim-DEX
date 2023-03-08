26244f754490df8beee7476b687eec91c3ba2517
Agrim <agrimsharma1714@gmail.com>
2023-03-08 23:52:22 +0530

    constructor in contract, bc holi, I didn't do more

--- hardhat-project/contracts/Exchange.sol
+++ hardhat-project/contracts/Exchange.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Exchange {
-    
+contract Exchange is ERC20 {
+    // Exchange is inheriting ERC20, because our exchange would keep track of Crypto Dev LP tokens
+    constructor(address _CryptoDevtoken) ERC20("CryptoDev LP Token", "CDLP") {
+        require(_CryptoDevtoken != address(0), "Token address passed is a null address");
+        cryptoDevTokenAddress = _CryptoDevtoken;
+    }
 }
